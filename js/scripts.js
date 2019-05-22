ac1cd41d5524f82484471c4b49a3fb050af241a3
Sidronio Lima <sidronio@sidroniolima.com.br>
2019-05-21 21:00:00 -0300

    NPM Scripts Part 2

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$("#carouselButton").click(function(){
+  if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+      $("#mycarousel").carousel('pause');
+      $("#carouselButton").children("span").removeClass('fa-pause');
+      $("#carouselButton").children("span").addClass('fa-play');
+  }
+  else if ($("#carouselButton").children("span").hasClass('fa-play')){
+      $("#mycarousel").carousel('cycle');
+      $("#carouselButton").children("span").removeClass('fa-play');
+      $("#carouselButton").children("span").addClass('fa-pause');                    
+  }
+});
+
+$("#button-sign-in").click(function() {
+  $("#loginModal").modal('toggle');
+});
+
+$("#button-reserve").click(function() {
+  $("#reservationModal").modal('toggle');
+});
